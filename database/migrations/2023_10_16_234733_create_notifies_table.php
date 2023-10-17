7c62195cab14b18f0c9fff0ebc9f1d388a24c358
sipinto <chairmankmj@gmail.com>
2023-10-17 16:10:48 +0900

    test code commit

--- database/migrations/2023_10_16_234733_create_notifies_table.php
+++ database/migrations/2023_10_16_234733_create_notifies_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('stuId');
             $table->string('check',1)->default('N');
-            $table->string('pId');
+            $table->string('pId', 100);
             $table->timestamps();
 
             $table->foreign('stuId')->references('stuId')->on('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('pId')->references('pId')->on('program')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('pId')->references('pId')->on('programs')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
