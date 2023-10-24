3550d40423e0b7ed3b2b270607353fe5757c828e
sipinto <chairmankmj@gmail.com>
2023-10-24 23:11:58 +0900

    user comlum check

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'studId',
+        'stuId',
         'email',
         'password',
         'grade',
