743cf864e737470c00506ea1c26c3fd861616c38
sipinto <chairmankmj@gmail.com>
2023-10-26 11:20:30 +0900

    program method auth divine

--- routes/api.php
+++ routes/api.php
@@ -33,48 +33,50 @@ Route::middleware('cors')->group(function(){
 Route::post('/regist', [RegisterController::class,'regist']);
 Route::post('/login', [LoginController::class, 'login']);
 
-// 회원 정보 (받기 / 수정 / 삭제)
-Route::prefix('user')->group(function () {
-    // 받기
-    Route::get('/info', [UserController::class, 'getInfo']);
-    // 수정
-    Route::put('/update/{id}', [UserController::class, 'update']);
-    // 삭제
-    Route::delete('/delete/{id}', [UserController::class, 'delete']);
-    // 리스트 받기
-    Route::get('/list',[UserController::class, 'getUserList'])
-    ->middleware('role:admin');
-});
 
-// 로그인 확인용 미들웨어 (유저, 관리자)
+// 토큰(로그인) 필요 미들웨어 (유저, 관리자)
+// 넣어주세요...
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
     // 알림
     Route::apiResource('/notify', NotifyController::class)->except([
         'create', 'show', 'edit'
     ]);
+    // 회원 정보 (받기 / 수정 / 삭제)
+    Route::prefix('user')->group(function () {
+        // 받기
+        Route::get('/info', [UserController::class, 'getInfo']);
+        // 수정
+        Route::put('/update/{id}', [UserController::class, 'update']);
+        // 삭제
+        Route::delete('/delete/{id}', [UserController::class, 'delete']);
+        // 리스트 받기
+        Route::get('/list',[UserController::class, 'getUserList'])
+        ->middleware('role:admin');
+    });
 
     // 관리자, 유저 로그아웃 통합 
     Route::post('/logout', [LoginController::class, 'logout']);
 
-    
-
-    // 관리자 (등록 / 삭제), 총관리자 변경
-    Route::apiResource('/admin', AdminController::class)->except([
-        'create','edit'
-    ]);
-
     // 관리자 권한 확인용 미들웨어
     Route::middleware(['role:admin'])->group(function() {
         // 여기에 관리자용 api 담아주시면 됩니다..
+        
+        // 관리자 (등록 / 삭제), 총관리자 변경
+        Route::apiResource('/admin', AdminController::class)->except([
+            'create','edit'
+        ]);
+
+        // 프로그램 (토큰 o 작업)
+        Route::apiResource('program', ProgramController::class)->except([
+            'create', 'show', 'edit','index','programInfo'
+        ]);
     });
 });
 
-// 프로그램
-Route::apiResource('program', ProgramController::class)->except([
-    'create', 'show', 'edit'
-]);
+
+// 프로그램 (토큰 x 작업)
 Route::post('program/programInfo/{pId}', [ProgramController::class, 'programInfo']);
+Route::apiResource('program', ProgramController::class)->only('index');
 
 // 지원하기
 Route::apiResource('applicant', ApplicantController::class)->except([
